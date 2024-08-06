15cedd0fc154e6b1a48569de71084cb2d5126203
NaoyaYamamoto <175591871+tarochan100@users.noreply.github.com>
2024-08-06 15:13:49 +0900

    新規Javaプロジェクト作成

---
+++ src/practice/Chapter5.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Chapter5{
+
+  public static void main(String[] args) {
+
+    // プログラムを作成
+
+  }
+
+}
---
+++ src/practice/Task.java
@@ -0,0 +1,6 @@
+package practice;
+
+public class Task {
+  //プログラムを作成
+	
+}
