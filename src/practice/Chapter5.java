3b135223813ecac2d70fc5dfea1397a5f7d2efdf
NaoyaYamamoto <175591871+tarochan100@users.noreply.github.com>
2024-08-07 17:32:00 +0900

    課題提出

--- src/practice/Chapter5.java
+++ src/practice/Chapter5.java
@@ -1,12 +1,31 @@
 package practice;
 
-public class Chapter5{
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-  public static void main(String[] args) {
+public class Chapter5 {
+    public static void main(String[] args) {
+    	
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy年M月d日");
 
-    // プログラムを作成
-
-	  
-  }
-
+        // Listの作成
+        List<Task> tasks = new ArrayList<>();
+        // 要素の追加
+        tasks.add(new Task(LocalDate.parse("2021年10月21日", formatter), "牛乳を買う。"));
+        tasks.add(new Task(LocalDate.parse("2021年9月15日", formatter), "○○社面談。"));
+        tasks.add(new Task(LocalDate.parse("2021年12月4日", formatter), "手帳を買う。"));
+        tasks.add(new Task(LocalDate.parse("2021年8月10日", formatter), "散髪に行く。"));
+        tasks.add(new Task(LocalDate.parse("2021年11月9日", formatter), "スクールの課題を解く。"));
+        
+        // 日付順に並び替え
+        Collections.sort(tasks);
+        
+        // 並び替えたタスクのリストを表示
+        for (Task task : tasks) {
+            System.out.println(task);
+        }
+    }
 }
--- src/practice/Task.java
+++ src/practice/Task.java
@@ -1,6 +1,31 @@
 package practice;
 
-public class Task {
-  //プログラムを作成
-	
+import java.time.LocalDate;
+
+public class Task implements Comparable<Task> {
+    private LocalDate date;
+    private String content;
+
+    public Task(LocalDate date, String content) {
+        this.date = date;
+        this.content = content;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+    
+    @Override
+    public int compareTo(Task other) {
+        return this.date.compareTo(other.date);
+    }
+
+    @Override
+    public String toString() {
+        return date + " : " + content;
+    }
 }
